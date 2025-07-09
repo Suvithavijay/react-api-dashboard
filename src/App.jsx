63cdaf9eae89a02ae824d1020fe59bacbc57a950
Suvithavijay <48257970+Suvithavijay@users.noreply.github.com>
2025-07-09 17:46:59 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactApiDashboard from "./components/ReactApiDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <ReactApiDashboard />
+    </div>
+  );
+}
+
+export default App;
